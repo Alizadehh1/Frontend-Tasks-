4ab0da730fdc58723795d6545bf8f831c4ad30cb
Alizadehh1 <alisal@code.edu.az>
2022-03-05 20:06:50 +0400

    first commit

---
+++ assets/javascript/slider.js
@@ -0,0 +1,43 @@
+document.querySelector('.slider-prev').addEventListener('click', function() {
+document.querySelector('.slick-prev').click();
+});
+
+document.querySelector('.slider-prev2').addEventListener('click', function() {
+document.querySelector('.slick-prev').click();
+});
+
+document.querySelector('.slider-next').addEventListener('click', function() {
+document.querySelector('.slick-next').click();
+});
+
+document.querySelector('.slider-next2').addEventListener('click', function() {
+document.querySelector('.slick-next').click();
+});
+$('.banner-slider-container').slick({
+    dots: true,
+    infinite: true,
+    speed: 500,
+    fade: true,
+    cssEase: 'linear',
+    autoplay: true,
+    autoplaySpeed: 2000,
+});
+document.querySelector('.arrowPrev').addEventListener('click', function() {
+document.querySelector('.product-slider').firstChild.click();
+});
+document.querySelector('.arrowNext').addEventListener('click', function() {
+document.querySelector('.product-slider').lastChild.click();
+});
+$('.product-slider').slick({
+    infinite: true,
+    slidesToShow: 10,
+    slidesToScroll: 1
+});
+
+
+
+$('.daily-best-sells-slider').slick({
+    infinite: true,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+});
